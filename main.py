d71fd88f7566917680c62340a30b45135067c215
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2021-01-06 21:38:08 -0500

    Updated import modules

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 
-from settings import Settings
-from twitchbot import TwitchBot
+from bot.settings import Settings
+from bot.twitchbot import TwitchBot
+
 
 config = Settings()
-
 
 event_loop = asyncio.get_event_loop()
 TwitchBot(config, event_loop).run()
