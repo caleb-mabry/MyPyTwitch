58b966845e5bc653cd29eda34c3932d1cc28fa31
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2021-01-06 23:23:37 -0500

    Changed to support new changes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import asyncio
 
 from bot.settings import Settings
 from bot.twitchbot import TwitchBot
-
+from bot.socket import Socket
 
 config = Settings()
-
+web_socket = Socket(config)
 event_loop = asyncio.get_event_loop()
-TwitchBot(config, event_loop).run()
+TwitchBot(web_socket, event_loop).run()
